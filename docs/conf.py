20a8cfc8e776df175955de2c731271710e686d88
Eric Holscher <eric@ericholscher.com>
2015-04-22 16:23:57 -0700

    Use autoapi. Meta.

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,10 @@ import shlex
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = ['autoapi.extension']
+
+autoapi_type = 'python'
+autoapi_dir = '../autoapi'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
--- docs/index.rst
+++ docs/index.rst
@@ -14,6 +14,7 @@ Contents
    config
    templates
    design
+   autoapi/index
 
 Install
 -------
