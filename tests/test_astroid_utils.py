8256daa09042e15906f1840e81828e0e62578257
Ashley Whetter <ashley@awhetter.co.uk>
2020-10-07 22:18:43 -0700

    Fixed test setup

--- tests/test_astroid_utils.py
+++ tests/test_astroid_utils.py
@@ -99,3 +99,54 @@ class TestAstroidUtils(object):
         node = astroid.extract_node(source)
         value = astroid_utils.get_assign_value(node)
         assert value == expected
+
+    @pytest.mark.parametrize(
+        "signature,expected",
+        [
+            ("a: bool, b: int = 5", {"a": "bool", "b": "int"}),
+            ("a: bool, /, b: int, *, c: str", {"a": "bool", "b": "int", "c": "str"}),
+            (
+                "a: bool, /, b: int, *args, c: str, **kwargs",
+                {"a": "bool", "b": "int", "c": "str"},
+            ),
+            ("a: int, *args, b: str, **kwargs", {"a": "int", "b": "str"}),
+        ],
+    )
+    def test_parse_annotations(self, signature, expected):
+        node = astroid.extract_node(
+            """
+            def func({}) -> str: #@
+                pass
+        """.format(
+                signature
+            )
+        )
+
+        annotations = astroid_utils.get_annotations_dict(node.args)
+        assert annotations == expected
+
+    @pytest.mark.parametrize(
+        "signature,expected",
+        [
+            ("a: bool, b: int = 5, c='hi'", "a: bool, b: int = 5, c='hi'"),
+            ("a: bool, /, b: int, *, c: str", "a: bool, /, b: int, *, c: str"),
+            (
+                "a: bool, /, b: int, *args, c: str, **kwargs",
+                "a: bool, /, b: int, *args, c: str, **kwargs",
+            ),
+            ("a: int, *args, b: str, **kwargs", "a: int, *args, b: str, **kwargs"),
+            ("a: 'A'", "a: A"),
+        ],
+    )
+    def test_format_args(self, signature, expected):
+        node = astroid.extract_node(
+            """
+            def func({}) -> str: #@
+                pass
+        """.format(
+                signature
+            )
+        )
+
+        formatted = astroid_utils.format_args(node.args)
+        assert formatted == expected
