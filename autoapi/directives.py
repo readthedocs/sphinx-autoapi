67c0b5f821c51fc40f94ec64b2dd90fe06b68563
Ashley Whetter <ashley@awhetter.co.uk>
2024-03-25 19:26:57 -0700

    Fix type checking against latest Sphinx

--- autoapi/directives.py
+++ autoapi/directives.py
@@ -48,7 +48,7 @@ class NestedParse(Directive):
     duplicate headings on sections.
     """
 
-    has_content = 1
+    has_content = True
     required_arguments = 0
     optional_arguments = 0
     final_argument_whitespace = False
